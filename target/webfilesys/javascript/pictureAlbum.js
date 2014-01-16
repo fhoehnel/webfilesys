715f7d86e0621e37780ec423d7df98de5d39fe8d
Frank Hoehnel <frank_hoehnel@hotmail.com>
2014-01-16 22:07:47 +0100

    release 2.9.4

--- target/webfilesys/javascript/pictureAlbum.js
+++
@@ -1,9 +0,0 @@
-    function albumImg(imgName)
-    {
-        window.location.href = '/webfilesys/servlet?command=albumImg&imgName=' + encodeURIComponent(imgName) + '&windowWidth=' + getWinWidth() + '&windowHeight=' + getWinHeight();
-    }
-
-    function albumLinkedImg(realPath)
-    {
-        window.location.href = '/webfilesys/servlet?command=albumImg&realPath=' + encodeURIComponent(realPath) + '&windowWidth=' + getWinWidth() + '&windowHeight=' + getWinHeight();
-    }
