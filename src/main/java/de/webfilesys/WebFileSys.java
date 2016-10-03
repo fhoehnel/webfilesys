4cd48e5e9bb0e6700207aad5359331dae45a2a86
fhoehnel <frank_hoehnel@hotmail.com>
2016-10-03 16:04:42 +0200

    Release 2.15.0

--- src/main/java/de/webfilesys/WebFileSys.java
+++ src/main/java/de/webfilesys/WebFileSys.java
@@ -44,7 +44,7 @@ public class WebFileSys
 {
 	private static WebFileSys instance = null;
 
-	public static final String VERSION = "Version 2.14.1-beta6 (23 Sep 2016)";
+	public static final String VERSION = "Version 2.15 (03 Oct 2016)";
  
     public static final String THUMB_DIR = "thumbnails";
 
