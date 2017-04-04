abe110731db5a0cb02611ac2440c49dec477d459
fhoehnel <frank_hoehnel@hotmail.com>
2017-04-04 21:52:02 +0200

    Fix custom confirm dialog

--- src/main/java/de/webfilesys/WebFileSys.java
+++ src/main/java/de/webfilesys/WebFileSys.java
@@ -44,7 +44,7 @@ public class WebFileSys
 {
 	private static WebFileSys instance = null;
 
-	public static final String VERSION = "Version 2.16.1-beta4 (30 Mar 2017)";
+	public static final String VERSION = "Version 2.16.1-beta5 (04 Apr 2017)";
  
     public static final String THUMB_DIR = "thumbnails";
 
