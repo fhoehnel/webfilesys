34e90e82874ad653c299f7df147babcbb93bc28a
fhoehnel <frank_hoehnel@hotmail.com>
2017-06-05 21:57:08 +0200

    fix javascript bug in self edit user profile

--- src/main/java/de/webfilesys/WebFileSys.java
+++ src/main/java/de/webfilesys/WebFileSys.java
@@ -44,7 +44,7 @@ public class WebFileSys
 {
 	private static WebFileSys instance = null;
 
-	public static final String VERSION = "Version 2.17.1-beta16 (01 June 2017)";
+	public static final String VERSION = "Version 2.18.0 (06 June 2017)";
  
     public static final String THUMB_DIR = "thumbnails";
 
