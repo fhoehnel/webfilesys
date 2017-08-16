0ac635b52cc3812b4ecc099f7eef8cae94e12096
fhoehnel <frank_hoehnel@hotmail.com>
2017-08-16 21:28:50 +0200

    version number

--- src/main/java/de/webfilesys/WebFileSys.java
+++ src/main/java/de/webfilesys/WebFileSys.java
@@ -44,7 +44,7 @@ public class WebFileSys
 {
 	private static WebFileSys instance = null;
 
-	public static final String VERSION = "Version 2.18.0 (06 June 2017)";
+	public static final String VERSION = "Version 2.19.0-beta1 (16 Aug 2017)";
  
     public static final String THUMB_DIR = "thumbnails";
 
