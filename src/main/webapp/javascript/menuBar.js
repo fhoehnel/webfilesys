e2c0ec3e01bb074ee5ec1a16684cacab09f33cf0
Frank Höhnel <Frank.Hoehnel@t-systems.com>
2021-03-17 10:54:56 +0100

    Menubar code quality improvement

--- src/main/webapp/javascript/menuBar.js
+++ src/main/webapp/javascript/menuBar.js
@@ -41,3 +41,99 @@ function gotoDirectPath() {
         }
     });
 }
+
+function setScreenSize() {
+    const parameters = { "screenWidth": screen.width, "screenHeight": screen.height };
+	xmlPostRequest("setScreenSize", parameters, () => {});
+}
+
+function refreshDriveList() {
+    const parameters = { "method": "refreshDriveList" };
+    
+	xmlGetRequest("ajaxRPC", parameters, function(responseXml) {
+        const resultItem = responseXml.getElementsByTagName("result")[0];
+        setTimeout(() => parent.location.href = "/webfilesys/servlet", 200);    
+    });
+}
+
+function unixCmdWin() {
+    const unixCmdWin = window.open('/webfilesys/servlet?command=unixCmdLine','cmdPrompt','status=no,toolbar=no,menu=no,width=600,height=600,resizable=yes,scrollbars=yes,left=10,top=10,screenX=10,screenY=10');
+    unixCmdWin.focus();
+}
+
+function fileSysStats() {
+    const statWin = window.open('/webfilesys/servlet?command=fileSysUsage','statWin','scrollbars=yes,resizable=yes,width=700,height=480,left=20,top=20,screenX=20,screenY=20');
+    statWin.focus();
+}
+
+function mobileVersion() {
+    parent.location.href = '/webfilesys/servlet?command=mobile&cmd=folderFileList&initial=true&relPath=/';
+}
+
+function returnToPrevDir() {
+    parent.location.href = '/webfilesys/servlet?command=returnToPrevDir';
+}
+
+function openCalendar() {
+    let windowWidth = screen.availWidth - 10;
+    let windowHeight = screen.availHeight - 20;
+  
+    if (browserChrome) {
+        windowHeight = windowHeight - 40;
+    }
+  
+    if (windowHeight > windowWidth - 200) {
+        windowHeight = windowWidth - 200;
+    } else if (windowWidth > windowHeight + 250) {
+        windowWidth = windowHeight + 250;
+    }
+
+    const calWin = window.open('/webfilesys/servlet?command=calendar','calWin','scrollbars=yes,resizable=yes,width=' + windowWidth + ',height=' + windowHeight);
+    calWin.focus();
+}
+
+function searchParms() {
+    const searchWin = window.open('/webfilesys/servlet?command=search','searchWin','scrollbars=yes,resizable=yes,width=500,height=480,left=100,top=60,screenX=100,screenY=60');
+    searchWin.focus();
+}
+
+function ftpBackup() {
+    const ftpWin = window.open('/webfilesys/servlet?command=ftpBackup','ftpWin','status=no,toolbar=no,menu=no,width=520,height=300,resizable=no,scrollbars=yes,left=150,top=60,screenX=150,screenY=60');
+    ftpWin.focus();
+}
+
+function slideshow() {
+    parent.frames[2].location.href = '/webfilesys/servlet?command=slideShowParms&cmd=getParms&screenWidth=' + screen.width + '&screenHeight=' + screen.height;
+}
+
+function pictureStory() {
+    let winWidth = 900;
+    if (winWidth > screen.width - 40) {
+        winWidth = screen.width - 40;
+    }
+    const xpos = (screen.width - 40 - winWidth) / 2;
+    const thumbwin = window.open('/webfilesys/servlet?command=pictureStory&initial=true', 'thumbwin', 'status=no,toolbar=no,menu=no,width=' + (winWidth) + ',height=' + (screen.height - 120) + ',resizable=yes,scrollbars=yes,left=' + xpos + ',top=1,screenX=' + xpos + ',screenY=1');
+    thumbwin.focus();
+}
+
+function diskQuota() {
+    const quotaWin = window.open('/webfilesys/servlet?command=diskQuota','quotaWin','scrollbars=no,resizable=no,width=400,height=230,left=100,top=100,screenX=100,screenY=100');
+    quotaWin.focus();
+}
+
+function publishList() {
+    const publishWin = window.open('/webfilesys/servlet?command=publishList','PublishList','scrollbars=yes,resizable=yes,width=800,height=400,left=20,top=100,screenX=20,screenY=100');
+    publishWin.focus();
+}
+
+function watchList() {
+   parent.DirectoryPath.location.href = '/webfilesys/servlet?command=watchList';
+}
+
+function bookmarks() {
+    parent.DirectoryPath.location.href = '/webfilesys/servlet?command=bookmarks';
+}
+
+function fastpath() {
+    parent.DirectoryPath.location.href = '/webfilesys/servlet?command=fastpath';
+}
